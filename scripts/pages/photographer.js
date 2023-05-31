620ab1d3f4b50a24352d8a9eb0beae2e573568a1
DjennadRany <128905298+DjennadRany@users.noreply.github.com>
2023-05-31 16:11:11 +0200

    Add files via upload

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -91,252 +91,278 @@ fetch('./data/photographers.json')
   });
 
 
-async function getPhotographers() {
-  try {
-    const response = await fetch('./data/photographers.json');
-    if (!response.ok) {
-      throw new Error('Looks like there was a problem. Status Code: ' + response.status);
-    }
-    const data = await response.json();
-    console.log(data.photographers);
+  async function getPhotographers() {
+    try {
+      const response = await fetch('./data/photographers.json');
+      if (!response.ok) {
+        throw new Error('Looks like there was a problem. Status Code: ' + response.status);
+      }
+      const data = await response.json();
+      console.log(data.photographers);
 
-    // Get the photographer ID from the URL
-    const url = window.location.href;
-    const id = url.split("#")[1];
-    console.log(id);
+      // Get the photographer ID from the URL
+      const url = window.location.href;
+      const id = url.split("#")[1];
+      console.log(id);
 
-    // Filter media based on photographer ID
-    const filteredMedia = data.media.filter(media => media.photographerId === parseInt(id));
-    console.log(filteredMedia);
+      // Filter media based on photographer ID
+      const filteredMedia = data.media.filter(media => media.photographerId === parseInt(id));
+      console.log(filteredMedia);
 
-    // Function to render a slide
-    function showSlide(index) {
-      const media = filteredMedia[index];
+      // Function to render a slide
+      function showSlide(index) {
+        const media = filteredMedia[index];
 
-      const article = document.createElement('div');
-      article.classList.add('blockouf');
-      const figure = document.createElement('figure');
-      const title = document.createElement('p');
-      const likes = document.createElement('p');
-      const date = document.createElement('span');
-      const price = document.createElement('span');
-      const prblock = document.createElement('div');
-      likes.classList.add('lowgne');
-      title.classList.add('ligne');
-      prblock.classList.add('prblock')
-      title.textContent = media.title;
-      likes.textContent = media.likes + '❤';
+        const article = document.createElement('div');
+        article.classList.add('blockouf');
+        const figure = document.createElement('figure');
+        const title = document.createElement('p');
+        const likes = document.createElement('p');
+        const date = document.createElement('span');
+        const price = document.createElement('span');
+        const prblock = document.createElement('div');
+        likes.classList.add('lowgne');
+        title.classList.add('ligne');
+        prblock.classList.add('prblock')
+        title.textContent = media.title;
+        likes.textContent = media.likes + '❤';
 
+        if (media.image) {
+          const image = document.createElement('img');
+          image.src = `./assets/${media.photographerId}/${media.image}`;
+          image.alt = media.title;
+          figure.appendChild(image);
+        } else {
+          const blackSquare = document.createElement('div');
+          blackSquare.classList.add('black-square');
 
-      if (media.image) {
-        const image = document.createElement('img');
-        image.src = `./assets/${media.photographerId}/${media.image}`;
-        image.alt = media.title;
-        figure.appendChild(image);
-      } else {
-        const blackSquare = document.createElement('div');
-        blackSquare.classList.add('black-square');
+          const playIcon = document.createElement('span');
+          playIcon.classList.add('play-icon');
+          playIcon.textContent = '▶';
 
-        const playIcon = document.createElement('span');
-        playIcon.classList.add('play-icon');
-        playIcon.textContent = '▶';
+          blackSquare.appendChild(playIcon);
+          figure.appendChild(blackSquare);
+        }
 
-        blackSquare.appendChild(playIcon);
-        figure.appendChild(blackSquare);
+        if (media.video && media.videoPoster) {
+          const video = document.createElement('video');
+          video.src = `./assets/${media.photographerId}/${media.video}`;
+          video.poster = `./assets/${media.photographerId}/${media.videoPoster}`;
+          video.alt = media.title;
+          figure.appendChild(video);
+        }
+
+        article.appendChild(figure);
+        article.appendChild(prblock);
+        prblock.appendChild(title);
+        prblock.appendChild(likes);
+        prblock.appendChild(date);
+        prblock.appendChild(price);
+
+        figure.addEventListener('click', () => {
+          openImageModal(index);
+        });
+
+        const sliderContainer = document.querySelector('.photographer_section');
+        sliderContainer.appendChild(article);
       }
 
-      if (media.video && media.videoPoster) {
-        const video = document.createElement('video');
-        video.src = `./assets/${media.photographerId}/${media.video}`;
-        video.poster = `./assets/${media.photographerId}/${media.videoPoster}`;
-        video.alt = media.title;
-        figure.appendChild(video);
+      // Render all the slides
+      function renderSlides() {
+        const sliderContainer = document.querySelector('.photographer_section');
+        sliderContainer.innerHTML = '';
+
+        filteredMedia.forEach((media, index) => {
+          showSlide(index);
+        });
       }
 
-      article.appendChild(figure);
-      article.appendChild(prblock);
-      prblock.appendChild(title);
+      // Function to open the image modal
+      function openImageModal(index) {
+        const existingModal = document.querySelector('.image-modal');
+        if (existingModal) {
+          closeModal();
+        }
 
+        const modal = document.createElement('div');
+        modal.classList.add('image-modal');
 
-      prblock.appendChild(likes);
-      prblock.appendChild(date);
-      prblock.appendChild(price);
+        const modalImage = document.createElement('img');
+        modalImage.src = `./assets/${filteredMedia[index].photographerId}/${filteredMedia[index].image}`;
+        modalImage.alt = filteredMedia[index].title;
 
-      figure.addEventListener('click', () => {
-        openImageModal(index);
-      });
+        const modalVideo = document.createElement('video');
+        modalVideo.src = `./assets/${filteredMedia[index].photographerId}/${filteredMedia[index].video}`;
+        modalVideo.poster = `./assets/${filteredMedia[index].photographerId}/${filteredMedia[index].videoPoster}`;
+        modalVideo.alt = filteredMedia[index].title;
+        modalVideo.controls = true;
+        modalVideo.autoplay = true;
 
-      const sliderContainer = document.querySelector('.photographer_section');
-      sliderContainer.appendChild(article);
-    }
+        const prevButton = document.createElement('button');
+        prevButton.textContent = 'Précédent';
+        prevButton.classList.add('prev-button');
+        prevButton.addEventListener('click', () => {
+          const newIndex = (index - 1 + filteredMedia.length) % filteredMedia.length;
+          closeModal();
+          openImageModal(newIndex);
+        });
 
-    // Render all the slides
-    function renderSlides() {
-      const sliderContainer = document.querySelector('.photographer_section');
-      sliderContainer.innerHTML = '';
+        const nextButton = document.createElement('button');
+        nextButton.textContent = 'Suivant';
+        nextButton.classList.add('next-button');
+        nextButton.addEventListener('click', () => {
+          const newIndex = (index + 1) % filteredMedia.length;
+          closeModal();
+          openImageModal(newIndex);
+        });
 
-      filteredMedia.forEach((media, index) => {
+        const closeButton = document.createElement('button');
+        closeButton.textContent = 'Fermer';
+        closeButton.classList.add('close-button');
+        closeButton.addEventListener('click', () => {
+          closeModal();
+        });
+
+        modal.appendChild(closeButton);
+        modal.appendChild(prevButton);
+        modal.appendChild(nextButton);
+
+        if (filteredMedia[index].video) {
+          modal.appendChild(modalVideo);
+        } else {
+          modal.appendChild(modalImage);
+        }
+
+        document.body.appendChild(modal);
+      }
+
+      // Function to close the image modal
+      function closeModal() {
+        const modal = document.querySelector('.image-modal');
+        if (modal) {
+          modal.remove();
+        }
+      }
+      function addFilterSection() {
+        // Handle filter change event
+        function handleFilterChange() {
+          const selectedValue = dropdownOptions.querySelector('.selected').getAttribute('data-value');
+      
+          if (selectedValue === 'date') {
+            filterMediaByDate();
+          } else if (selectedValue === 'title') {
+            filterMediaByTitle();
+          } else if (selectedValue === 'popularity') {
+            filterMediaByPopularity();
+          }
+        }
+      
+        // Filter media by date
+        function filterMediaByDate() {
+          filteredMedia.sort((a, b) => new Date(a.date) - new Date(b.date));
+          renderSlides();
+        }
+      
+        // Filter media by title
+        function filterMediaByTitle() {
+          filteredMedia.sort((a, b) => a.title.localeCompare(b.title));
+          renderSlides();
+        }
+      
+        // Filter media by popularity
+        function filterMediaByPopularity() {
+          filteredMedia.sort((a, b) => b.likes - a.likes);
+          renderSlides();
+        }
+      
+        // Create filter section elements
+        const filterSection = document.createElement('nav');
+        filterSection.classList.add('filter-section');
+      
+        const filterLabel = document.createElement('label');
+        filterLabel.setAttribute('for', 'filter-select');
+        filterLabel.textContent = 'Trier par :';
+      
+        const dropdownContainer = document.createElement('div');
+        dropdownContainer.classList.add('dropdown');
+      
+        const dropdownLabel = document.createElement('span');
+        dropdownLabel.classList.add('dropdown-label');
+        dropdownLabel.textContent = 'Date';
+      
+        const dropdownIcon = document.createElement('span');
+        dropdownIcon.classList.add('dropdown-icon');
+        dropdownIcon.innerHTML = '&#9662;';
+      
+        const dropdownOptions = document.createElement('ul');
+        dropdownOptions.classList.add('dropdown-options');
+      
+        const dropdownOptionDate = document.createElement('li');
+        dropdownOptionDate.textContent = 'Date';
+        dropdownOptionDate.setAttribute('data-value', 'date');
+        dropdownOptionDate.classList.add('selected');
+      
+        const dropdownOptionTitle = document.createElement('li');
+        dropdownOptionTitle.textContent = 'Titre';
+        dropdownOptionTitle.setAttribute('data-value', 'title');
+      
+        const dropdownOptionPopularity = document.createElement('li');
+        dropdownOptionPopularity.textContent = 'Popularité';
+        dropdownOptionPopularity.setAttribute('data-value', 'popularity');
+      
+        dropdownOptions.appendChild(dropdownOptionDate);
+        dropdownOptions.appendChild(dropdownOptionTitle);
+        dropdownOptions.appendChild(dropdownOptionPopularity);
+      
+        dropdownContainer.appendChild(dropdownLabel);
+        dropdownContainer.appendChild(dropdownIcon);
+        dropdownContainer.appendChild(dropdownOptions);
+      
+        filterSection.appendChild(filterLabel);
+        filterSection.appendChild(dropdownContainer);
+      
+        const sliderContainer = document.querySelector('.photographer_section');
+        sliderContainer.parentNode.insertBefore(filterSection, sliderContainer);
+      
+        // Toggle dropdown visibility and update selected value
+        dropdownContainer.addEventListener('click', function() {
+          dropdownOptions.classList.toggle('show');
+        });
+      
+        dropdownOptions.addEventListener('click', function(event) {
+          if (event.target.tagName === 'LI') {
+            const selectedValue = event.target.getAttribute('data-value');
+            dropdownOptions.querySelector('.selected').classList.remove('selected');
+            event.target.classList.add('selected');
+            const selectedText = event.target.textContent;
+            dropdownLabel.textContent = selectedText;
+            handleFilterChange();
+            dropdownOptions.classList.remove('show');
+          }
+        });
+      }
+      
+      
+      
+      
+      
+
+      // Render initial slides
+      filteredMedia.forEach((_media, index) => {
         showSlide(index);
       });
-    }
 
-    // Function to open the image modal
-    function openImageModal(index) {
-      const existingModal = document.querySelector('.image-modal');
-      if (existingModal) {
-        closeModal();
-      }
+      // Add the filter section
+      addFilterSection();
 
-      const modal = document.createElement('div');
-      modal.classList.add('image-modal');
-
-      const modalImage = document.createElement('img');
-      modalImage.src = `./assets/${filteredMedia[index].photographerId}/${filteredMedia[index].image}`;
-      modalImage.alt = filteredMedia[index].title;
-
-      const modalVideo = document.createElement('video');
-      modalVideo.src = `./assets/${filteredMedia[index].photographerId}/${filteredMedia[index].video}`;
-      modalVideo.poster = `./assets/${filteredMedia[index].photographerId}/${filteredMedia[index].videoPoster}`;
-      modalVideo.alt = filteredMedia[index].title;
-      modalVideo.controls = true;
-      modalVideo.autoplay = true;
-
-      const prevButton = document.createElement('button');
-      prevButton.textContent = 'Précédent';
-      prevButton.classList.add('prev-button');
-      prevButton.addEventListener('click', () => {
-        const newIndex = (index - 1 + filteredMedia.length) % filteredMedia.length;
-        closeModal();
-        openImageModal(newIndex);
-      });
-
-      const nextButton = document.createElement('button');
-      nextButton.textContent = 'Suivant';
-      nextButton.classList.add('next-button');
-      nextButton.addEventListener('click', () => {
-        const newIndex = (index + 1) % filteredMedia.length;
-        closeModal();
-        openImageModal(newIndex);
-      });
-
-      const closeButton = document.createElement('button');
-      closeButton.textContent = 'Fermer';
-      closeButton.classList.add('close-button');
-      closeButton.addEventListener('click', () => {
-        closeModal();
-      });
-
-      modal.appendChild(closeButton);
-      modal.appendChild(prevButton);
-      modal.appendChild(nextButton);
-
-      if (filteredMedia[index].video) {
-        modal.appendChild(modalVideo);
-      } else {
-        modal.appendChild(modalImage);
-      }
-
-      document.body.appendChild(modal);
-    }
-
-    // Function to close the image modal
-    function closeModal() {
-      const modal = document.querySelector('.image-modal');
-      if (modal) {
-        modal.remove();
-      }
-    }
-
-function addFilterSection() {
-  // Handle filter change event
-  function handleFilterChange() {
-    const selectElement = document.getElementById('filter-select');
-    const selectedValue = selectElement.value;
-
-    if (selectedValue === 'date') {
-      filterMediaByDate();
-    } else if (selectedValue === 'title') {
-      filterMediaByTitle();
-    } else if (selectedValue === 'popularity') {
-      filterMediaByPopularity();
+    } catch (error) {
+      console.error(error);
     }
   }
 
-  // Filter media by date
-  function filterMediaByDate() {
-    filteredMedia.sort((a, b) => new Date(a.date) - new Date(b.date));
-    renderSlides();
-  }
-
-  // Filter media by title
-  function filterMediaByTitle() {
-    filteredMedia.sort((a, b) => a.title.localeCompare(b.title));
-    renderSlides();
-  }
-
-  // Filter media by popularity
-  function filterMediaByPopularity() {
-    filteredMedia.sort((a, b) => b.likes - a.likes);
-    renderSlides();
-  }
-
-  // Create filter section elements
-  const filterSection = document.createElement('nav');
-  filterSection.classList.add('filter-section');
-
-  const filterLabel = document.createElement('label');
-  filterLabel.setAttribute('for', 'filter-select');
-  filterLabel.textContent = 'Trier par :';
-
-  const filterSelect = document.createElement('select');
-  filterSelect.setAttribute('id', 'filter-select');
-  filterSelect.setAttribute('aria-haspopup', 'true'); // Add aria-haspopup attribute
-
-  const dateOption = document.createElement('option');
-  dateOption.setAttribute('value', 'date');
-  dateOption.textContent = 'Date';
-
-  const titleOption = document.createElement('option');
-  titleOption.setAttribute('value', 'title');
-  titleOption.textContent = 'Titre';
-
-  const popularityOption = document.createElement('option');
-  popularityOption.setAttribute('value', 'popularity');
-  popularityOption.textContent = 'Popularité';
-
-  filterSelect.appendChild(dateOption);
-  filterSelect.appendChild(titleOption);
-  filterSelect.appendChild(popularityOption);
-
-  filterSection.appendChild(filterLabel);
-  filterSection.appendChild(filterSelect);
-
-  // Insert filter section before the slider container
-  const sliderContainer = document.querySelector('.photographer_section');
-  sliderContainer.parentNode.insertBefore(filterSection, sliderContainer);
-
-  // Add event listener for filter change
-  filterSelect.addEventListener('change', handleFilterChange);
-}
-
-
-    // Render initial slides
-    filteredMedia.forEach((media, index) => {
-      showSlide(index);
-    });
-
-    // Add the filter section
-    addFilterSection();
-
-
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-// Call the main function
-getPhotographers();
-
+  // Call the main function
+  getPhotographers();
+  
 function displayPhotographersData() {
   // Obtenir l'URL actuelle
   const url = window.location.href;
