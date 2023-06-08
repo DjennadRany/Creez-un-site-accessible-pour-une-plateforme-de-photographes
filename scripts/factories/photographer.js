c9dfce6a339cff4dc68f0f3d5f07e9264a6a503a
Djennad <128905298+DjennadRany@users.noreply.github.com>
2023-06-08 13:52:38 +0200

    Add files via upload

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:scripts/factories/photographer.js
 function photographerFactory(data) {
     const { name, portrait } = data;
 
@@ -15,22 +14,4 @@ function photographerFactory(data) {
         return (article);
     }
     return { name, picture, getUserCardDOM }
-=======
-function photographerTemplate(data) {
-    const { name, portrait } = data;
-
-    const picture = `./assets/photographers/${item.portrait}`;
-
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
->>>>>>> a383b40 (Message de commit):scripts/templates/photographer.js
 }
