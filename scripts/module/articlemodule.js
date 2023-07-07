9d292711d81d8cd3bdf3ae756287f98bfd92321d
Djennad <128905298+DjennadRany@users.noreply.github.com>
2023-07-07 16:06:07 +0200

    Add files via upload

--- scripts/module/articlemodule.js
+++ scripts/module/articlemodule.js
@@ -1,6 +1,7 @@
 export default class Article {
     constructor(media) {
       this.article = document.createElement('div');
+      this.sousart = document.createElement('div');
       this.article.classList.add('blockouf');
       this.article.setAttribute('tabindex', '0');
   
@@ -12,7 +13,8 @@ export default class Article {
       this.price = document.createElement('span');
   
       this.prblock = document.createElement('div');
-  
+    
+      this.sousart.classList.add('sousart');
       this.likes.classList.add('lowgne');
       this.title.classList.add('ligne');
       this.prblock.classList.add('prblock');
@@ -24,11 +26,12 @@ export default class Article {
   
       // Ajouter les éléments créés à l'élément article
       this.article.appendChild(this.figure);
-      this.article.appendChild(this.title);
-      this.article.appendChild(this.likes);
+      this.article.appendChild(this.sousart);
       this.article.appendChild(this.date);
       this.article.appendChild(this.price);
       this.article.appendChild(this.prblock);
+      this.sousart.appendChild(this.title);
+      this.sousart.appendChild(this.likes);
     }
   
 
