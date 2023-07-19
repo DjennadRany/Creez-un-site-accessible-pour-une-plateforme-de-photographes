9ea847fed0fe2966482c4f3b155735e471b1eb6a
Djennad <128905298+DjennadRany@users.noreply.github.com>
2023-07-19 18:53:48 +0200

    Delete script directory

--- script/app.js
+++
@@ -1,8 +0,0 @@
-import { RecipeApp } from './script.js';
-import { RecipeFilters } from './filter.js';
-
-// Créez une instance de la classe RecipeApp
-const app = new RecipeApp();
-
-// Créez une instance de la classe RecipeFilters
-const filter = new RecipeFilters();
